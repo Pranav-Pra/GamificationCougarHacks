276a7011c0e0728f4e4b187576c41ad8e29d9146
Pranav-Pra <68164506+Pranav-Pra@users.noreply.github.com>
2022-03-12 19:20:43 -0600

    employee join code screen

---
+++ app/src/main/java/com/example/gamification/EmployeeJoinCode.java
@@ -0,0 +1,14 @@
+package com.example.gamification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeJoinCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_join_code);
+    }
+}
